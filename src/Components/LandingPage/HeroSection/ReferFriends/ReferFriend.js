6148f51daba9c0b0c016e8b4750672d786fa2a1f
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-04 00:32:37 +0530

    ReferFriends Component

---
+++ src/Components/LandingPage/HeroSection/ReferFriends/ReferFriend.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './ReferFriend.module.css'
+
+export default function ReferFriend() {
+  return (
+    <div className={styles.ReferFriend}>
+      <h1 className={styles.ReferFriendText}>REFER YOUR FRIENDS</h1>
+        <img className={styles.ReferFriendImage} src='https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F8638d096-d168-404e-9b83-56967625b5e6.jpg&w=1920&q=75'/>
+    </div>
+  )
+}
