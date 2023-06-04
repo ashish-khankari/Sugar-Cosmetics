bc2f237642408bdccf49db4dd7768e40146a1f96
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-04 21:56:48 +0530

    Modified Hot Deals Section

--- src/Components/LandingPage/HeroSection/HotDeals/HotDeals.js
+++ src/Components/LandingPage/HeroSection/HotDeals/HotDeals.js
@@ -9,20 +9,18 @@ export default function HotDeals() {
         "https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F5be86440-bf1e-4ef0-a5e7-444dbcab1f15.jpg&w=1920&q=75",
         "https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F43543293-8fcb-41c9-9f2f-e4300a05bf54.jpg&w=1920&q=75",
         "https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F7cf99d84-b598-43d9-8346-6017d5019da9.jpg&w=1920&q=75",
-        "https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F0c2c52dd-b711-4534-a385-2e01855f03c2.gif&w=1920&q=75"
+        "https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F0c2c52dd-b711-4534-a385-2e01855f03c2.gif&w=1920&q=75",
+        "https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Fc5f21f3a-1b28-4ad2-9fca-32286e4bc01b.jpg&w=1920&q=75"
     ]
 
-    // const settings = {
-    //         dots: true,
-    //         infinite: true,
-    //         speed: 500,
-    //         slidesToShow: 3,
-    //         slidesToScroll: 1,
-    //         // autoplay: true, 
-    //         // autoplaySpeed: 2000, 
-    //         // fade: true, 
+    const settings = {
+            infinite: true,
+            speed: 500,
+            // autoplay: true, 
+            // autoplaySpeed: 2000, 
+            // fade: true, 
 
-    // }
+    }
 
     const responsive = {
         superLargeDesktop: {
@@ -35,7 +33,7 @@ export default function HotDeals() {
             items: 3
         },
         tablet: {
-            breakpoint: { max: 1024, min: 464 },
+            breakpoint: { max: 900, min: 600 },
             items: 2
         },
         mobile: {
@@ -44,18 +42,20 @@ export default function HotDeals() {
         }
     };
 
+    const products = HotDealsImages.map(item=>(
+        <img className={styles.images} src={item}/>
+    ))
     return (
-        <div>
+        <div className={styles.productImages}>
+            <div className={styles.heading}>
+            <h1 className={styles.text}>HOT DEALS</h1>
+
+            </div>
             <Carousel
-                responsive={responsive}
-                className={styles.cardss}
-                infinite={true}
+               {...settings} 
+               responsive={responsive}
             >
-                <div><img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2Fc5f21f3a-1b28-4ad2-9fca-32286e4bc01b.jpg&w=1920&q=75" className={styles.images} /></div>
-                <div><img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F7cf99d84-b598-43d9-8346-6017d5019da9.jpg&w=1920&q=75" className={styles.images} /></div>
-                <div><img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F5be86440-bf1e-4ef0-a5e7-444dbcab1f15.jpg&w=1920&q=75" className={styles.images} /></div>
-                <div><img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F43543293-8fcb-41c9-9f2f-e4300a05bf54.jpg&w=1920&q=75" className={styles.images} /></div>
-                <div><img src="https://in.sugarcosmetics.com/_next/image?url=https%3A%2F%2Fd32baadbbpueqt.cloudfront.net%2FHomepage%2F3bb1ac45-ec29-4d25-93e1-e7f2e88dab33.gif&w=1920&q=75" className={styles.images} /></div>
+                {products}
             </Carousel>;
 
         </div>
