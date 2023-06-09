52d158061e86a1d8a0d1159e275e63963dfb0459
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-10 01:51:39 +0530

    mapped eyes products data

--- src/Components/Section/Eyes/Eyes.js
+++ src/Components/Section/Eyes/Eyes.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './Eyes.module.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { getEyesData } from '../../../store/slices/productSlice'
+
 
 export default function Eyes() {
+  const dispatch = useDispatch()
+  const eyesData = useSelector(state => state.myProducts.eyesProduct)
+  console.log(eyesData)
+  useEffect(() => {
+    dispatch(getEyesData())
+  }, [])
+
   return (
-    <div>Eyes</div>
+    <div className={styles.products}>
+      {
+        eyesData.map((product) => (
+          <div className={styles.lipsProduct}>
+            <img className={styles.lipsImage} src={product.image_link} />
+            <p key={product.id} className={styles.productName}>{product.name}</p>
+
+            <button className={styles.lipsButton}>Add to Cart</button>
+          </div>
+        ))
+      }
+    </div>
   )
 }
