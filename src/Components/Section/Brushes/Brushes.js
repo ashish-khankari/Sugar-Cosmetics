c66ea50b3814c1bf209f888a116ad6b8b998c90c
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-10 01:52:01 +0530

    Mapped Brushes Data

--- src/Components/Section/Brushes/Brushes.js
+++ src/Components/Section/Brushes/Brushes.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getBlushData } from '../../../store/slices/productSlice'
+import styles from './Brushes.module.css'
+
 
 export default function Brushes() {
+  const dispatch = useDispatch()
+  const blushProducts = useSelector(state => state.myProducts.blushProduct)
+  console.log(blushProducts)
+  useEffect(() => {
+    dispatch(getBlushData())
+  }, [])
   return (
-    <div>Brushes</div>
+    <div className={styles.products}>
+      {blushProducts.map((product) => (
+        <div className={styles.lipsProduct}>
+          <img className={styles.lipsImage} src={product.image_link} />
+          <p key={product.id} className={styles.productName}>{product.name}</p>
+
+          <button className={styles.lipsButton}>Add to Cart</button>
+        </div>
+      ))}
+    </div>
   )
 }
