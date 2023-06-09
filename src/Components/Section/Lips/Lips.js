d25152e1bd885ddc00524b61ccb7d9c1bd0e3da6
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-10 01:52:41 +0530

    Added Products for Lips category

--- src/Components/Section/Lips/Lips.js
+++ src/Components/Section/Lips/Lips.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getLipsData } from '../../../store/slices/productSlice'
+import styles from './Lips.module.css'
 
 export default function Lips() {
+  const dispatch = useDispatch()
+  const lipsData = useSelector(state => state.myProducts.products)
+
+  useEffect(() => {
+    dispatch(getLipsData())
+  }, [])
   return (
-    <div>Lips</div>
+    <div className={styles.products}>
+      {lipsData.map((product) => (
+        <div className={styles.lipsProduct}>
+          <img className={styles.lipsImage} src={product.image_link}/>
+          <p key={product.id} className={styles.productName}>{product.name}</p>
+
+          <button className={styles.lipsButton}>Add to Cart</button>
+        </div>
+      ))}
+    </div>
   )
 }
