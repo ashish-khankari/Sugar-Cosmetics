a4ac0616dcf38c2a47720627a84427c855a7fd56
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-10 20:45:24 +0530

    Added link to Navbar and added some stylings to icons

--- src/Components/LandingPage/Header/Navbar/Navbar.js
+++ src/Components/LandingPage/Header/Navbar/Navbar.js
@@ -3,21 +3,23 @@ import styles from './Navbar.module.css'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { BiStoreAlt } from 'react-icons/bi'
 import { CiDiscount1 } from 'react-icons/ci'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div className={styles.container}>
       <div>
-        <img src='https://entrackr.com/storage/2022/03/Sugar.jpg' className={styles.sugarLogo} alt='logo' />
+        <Link to={'/'}><img src='https://entrackr.com/storage/2022/03/Sugar.jpg' className={styles.sugarLogo} alt='logo' /></Link>
+
       </div>
 
       <div className={styles.user}>
         Hi Sugar Fan
       </div>
       <div className={styles.icons}>
-        <AiOutlineHeart />
-        <BiStoreAlt />
-        <CiDiscount1 />
+        <AiOutlineHeart className={styles.icon} />
+        <Link to={'/cart'}><BiStoreAlt className={styles.icon} /></Link>
+        <CiDiscount1 className={styles.icon} />
       </div>
     </div>
   )
