7375063932cee02c1b56a541c933ab54c7406b6b
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-11 20:40:58 +0530

    dispatched products

--- src/Components/Section/Lips/Lips.js
+++ src/Components/Section/Lips/Lips.js
@@ -1,29 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getLipsData, addtoCart } from '../../../store/slices/productSlice'
+import { getLipsData, addtoCart, addtoFavourites } from '../../../store/slices/productSlice'
 import styles from './Lips.module.css'
+import { AiOutlineHeart } from 'react-icons/ai'
+
 
 export default function Lips() {
   const dispatch = useDispatch()
   const lipsData = useSelector(state => state.myProducts.products)
 
+  const [bookmarkColor, setbookmarkColor] = useState(false)
+
   useEffect(() => {
     dispatch(getLipsData())
   }, [])
 
-  function addProducttoCart(product){
+  function addProducttoCart(product) {
     dispatch(addtoCart(product))
   }
 
+  function addedToBookmarkList(product){
+    // setbookmarkColor(!bookmarkColor)
+    dispatch(addtoFavourites(product))
+  }
 
   return (
-    <div className={styles.products}>
-      {lipsData.map((product) => (
-        <div className={styles.lipsProduct}>
-          <img className={styles.lipsImage} src={product.image_link}/>
-          <p key={product.id} className={styles.productName}>{product.name}</p>
 
-          <button className={styles.lipsButton} onClick={()=>addProducttoCart(product)}>Add to Cart</button>
+    <div className={styles.products}>
+      <img src='https://sugar-mobile-application.s3.amazonaws.com/collection-web-banner/Lips.jpg' className={styles.titleImage}/>
+      {lipsData.map((product) => (
+
+        <div className={styles.lipsProduct}>
+          <div className={styles.bookmarkSign} onClick={()=>addedToBookmarkList(product)}><AiOutlineHeart /></div>
+          <img className={styles.lipsImage} src={product.image_link} />
+          <p key={product.id} className={styles.productName}>{product.name}</p>
+          <button className={styles.lipsButton} onClick={() => addProducttoCart(product)}>Add to Cart</button>
         </div>
       ))}
     </div>
