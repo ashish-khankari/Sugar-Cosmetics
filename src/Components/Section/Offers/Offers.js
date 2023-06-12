c7c68abc6f7ab4670464393f6fe69ae993d35a8c
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-12 22:27:50 +0530

    Added Offers Section

--- src/Components/Section/Offers/Offers.js
+++ src/Components/Section/Offers/Offers.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import styles from './Offers.module.css'
+import { Offersdata } from './OffersData'
 
 export default function Offers() {
   return (
-    <div>Offers</div>
+
+    <div className={styles.mainContainer}>
+      <div className={styles.heading}>
+        <h1 className={styles.text}>Offers</h1>
+      </div>
+
+      <div className={styles.container}>
+
+        {
+          Offersdata.map((item) => (
+            <div className={styles.imagesContainer}>
+              <img className={styles.images} src={item} />
+              <div>
+                <p className={styles.code}>Copy Code</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+
   )
 }
