1b67480d481cc02e5e2f8e80bd273277eedf03a9
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-12 22:30:21 +0530

    Added Array Data For Images for Offers Section

---
+++ src/Components/Section/Offers/OffersData.js
@@ -0,0 +1,14 @@
+export const Offersdata = [
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/d1f410e3-3869-4437-9807-294df7ba841d.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/3a48c4d4-1b27-43d7-8132-1b9c824b2b6d.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/b0ed206e-55b0-4a1a-9b56-d296d88d4ef1.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/912353f0-0542-4611-8906-926109fd1df0.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/6169ea19-68aa-470d-8d2a-a482d44446d8.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/cff5e1b9-591c-4fe6-8fca-fb34232766f4.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/f4a0720d-5696-400c-858b-654372f9b014.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/1151debe-9f91-4e7e-a8fd-36e4ffa9c2be.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/77c4ec17-7e89-4287-8ce5-331a2eb7911d.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/96bc2824-4603-46d6-a5a5-5c5c193c3a00.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/69f71094-4f38-436b-a23c-8efc0cd15df3.jpg",
+    "https://d32baadbbpueqt.cloudfront.net/Promotion/80b9cbdb-bab2-4e6d-907d-1d6abc293c07.jpg"
+]
