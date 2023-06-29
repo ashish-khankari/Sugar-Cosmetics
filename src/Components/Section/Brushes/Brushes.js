fb56aba8617ca68986d0ca6f12ba1bfbfa5f6772
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-29 19:35:54 +0530

    Modified Brushes Section

--- src/Components/Section/Brushes/Brushes.js
+++ src/Components/Section/Brushes/Brushes.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { addtoCart, getBlushData, addtoFavourites } from '../../../store/slices/productSlice'
+import { addtoCart, getBlushData, addtoFavourites, sortfromHightoLow, sortfromLowtoHigh, sortbyName } from '../../../store/slices/productSlice'
 import styles from './Brushes.module.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -41,46 +41,80 @@ export default function Brushes() {
     }
   }
 
-  function addtoBookmarkSection(product){
+  function addtoBookmarkSection(product) {
     dispatch(addtoFavourites(product))
+  }
+
+  function sortAlphabetically() {
+    dispatch(sortbyName())
+  }
+
+  function sortHightoLow() {
+    dispatch(sortfromHightoLow())
+  }
+
+  function sortLowtoHigh() {
+    dispatch(sortfromLowtoHigh())
   }
 
 
   return (
     <div className={styles.container}>
-    {
-      blushProducts.map((product) => (
-        <div className={styles.card}>
-          <div className={styles.imagess}>
-            <img src={product.image_link} className={styles.image} alt="" />
+      <div className={styles.innerContainer}>
+        <div className={styles.buttons}>
+          <div className={styles.radiobutton}>
+            <input type="radio" id="html" name="fav_language" value="HTML" onClick={sortAlphabetically} />
+            <lable>Name</lable>
           </div>
-          <div className={styles.cardBody}>
-            <div className={styles.row}>
-              <div className={styles.cardTitle}>
-                {/* <h4>{product.name}</h4> */}
-                <h3>${product.price}</h3>
-              </div>
-              <div className={styles.viewBtn}>
-                <a href="">View Details</a>
-              </div>
-            </div>
-            <hr />
-            <div className={styles.name}>
-              <h4>{product.name}</h4>
-            </div>
-            <div className={styles.btnGrp}>
-              <div className={styles.btn}>
-                <button onClick={() => addProducttoCart(product)}>Add to Cart</button>
-              </div>
 
-              <Like />
-            </div>
+          <div className={styles.radiobutton}>
+            <input type="radio" id="html" name="fav_language" value="HTML" onClick={sortHightoLow} />
+            <lable>Price - High to Low</lable>
           </div>
+
+          <div className={styles.radiobutton}>
+            <input type="radio" id="html" name="fav_language" value="HTML" onClick={sortLowtoHigh} />
+            <lable>Price - Low to High</lable>
+          </div>
+
         </div>
-      ))
-    }
-    <ToastContainer />
+        <div className={styles.mappedData}>
+          {
+            blushProducts.map((product) => (
+              <div className={styles.card}>
+                <div className={styles.imagess}>
+                  <img src={product.image_link} className={styles.image} alt="" />
+                </div>
+                <div className={styles.cardBody}>
+                  <div className={styles.row}>
+                    <div className={styles.cardTitle}>
+                      {/* <h4>{product.name}</h4> */}
+                      <h3>${product.price}</h3>
+                    </div>
+                    <div className={styles.viewBtn}>
+                      <a href="">View Details</a>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className={styles.name}>
+                    <h4>{product.name}</h4>
+                  </div>
+                  <div className={styles.btnGrp}>
+                    <div className={styles.btn}>
+                      <button onClick={() => addProducttoCart(product)}>Add to Cart</button>
+                    </div>
 
-  </div>
+                    <Like />
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+      <ToastContainer />
+
+    </div>
   )
 }
